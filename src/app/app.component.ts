77ab73bad4e249780da3ce96689f108c9587306e
darsay8 <darsay.one@gmail.com>
2025-01-12 21:35:28 -0300

    Add authentication guard and initialize MSAL in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MsalAuthService } from './auth/msal.service';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'med-monitor-app';
+  constructor(private msalAuthService: MsalAuthService) {
+    this.msalAuthService.initializeAuth();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PatientDetailComponent } from './components/patient-detail/patient-detail.component';
+import { MsalGuard } from '@azure/msal-angular';
+import { FailedComponent } from './components/failed/failed.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'patient/:id', component: PatientDetailComponent },
+  {
+    path: 'patient/:id',
+    component: PatientDetailComponent,
+    canActivate: [MsalGuard],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [MsalGuard],
+  },
+  {
+    path: 'login-failed',
+    component: FailedComponent,
+  },
 ];
